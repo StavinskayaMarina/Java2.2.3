848f299f717856702d5d071d750484344716c971
Stavinskaya_M <marina.st6277@gmail.com>
2023-04-17 18:11:45 +0600

    rework

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,11 +2,11 @@ import static java.lang.Math.pow;
 import static java.lang.Math.round;
 
 public class CreditPaymentService {
-    public double calculate(double s, int t, double p) {
-        int period = t * 12; // кол-во месяцев
-        double mr = p / (100 * 12); // процент месячног платежа
+    public double calculate(double total, int date, double ante) {
+        int period = date * 12; // кол-во месяцев
+        double monthlyPayment = ante / (100 * 12); // процент месячного платежа
 
-        double result = s * (mr / (1 - (pow(1 + mr, -period))));
+        double result = total * (monthlyPayment / (1 - (pow(1 + monthlyPayment, -period))));
 
 
         return round(result);
